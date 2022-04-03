d92c7cf630edb3325267b9f85b0a1ea3f13c2eef
Wasurat S <wasurat_me96@outlook.com>
2022-04-04 00:24:15 +0700

    Complete 1st trial on API by Fast API

--- prediction_api.py
+++ prediction_api.py
@@ -61,7 +61,8 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
 app = FastAPI()
 
 config = yaml.safe_load(open("./params.yml"))
-preprocess_config = config['process_data']['interfence_mode']
+preprocess_config = config['process_data']
+preprocess_interf_config = preprocess_config['interfence_mode']
 model_config = config['model']
 
 @app.get("/")
@@ -71,19 +72,44 @@ async def get_items():
 @app.post("/")
 async def inference(user_data: User):
     trained_model = load(config['model']['model_path'])
-    encoder = load(preprocess_config['encoder'])
-    label_binarize = load(preprocess_config['label_bin'])
+    encoder_model = load(preprocess_interf_config['encoder'])
+    label_binarize = load(preprocess_interf_config['label_bin'])
 
     input_array = np.array([[
         user_data.age,
         user_data.workclass,
         user_data.fnlgt,
         user_data.education,
-        user_data.education-num,
-        
-
-
-        ['age', 'workclass', 'fnlgt', 'education', 'education-num',
-       'marital-status', 'occupation', 'relationship', 'race', 'sex',
-       'hours-per-week', 'native-country', 'salary']
+        user_data.marital_status,
+        user_data.occupation,
+        user_data.relationship,
+        user_data.race,
+        user_data.sex,
+        user_data.hours_per_week,
+        user_data.native_country,
+        user_data.salary
     ]])
+
+    post_data_df = DataFrame(data = input_array, columns =[
+        "age",
+        "workclass",
+        "fnlgt",
+        "education",
+        "marital_status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "hours_per_week",
+        "native_country",
+        "salary"
+    ])
+
+    X_featurized, _, _, _ = process_data(post_data_df,
+                                        categorical_features = preprocess_config['cat_features'],
+                                        encoder=encoder_model,
+                                        lb = label_binarize,
+                                        training = preprocess_interf_config['train'])
+    preds_value = inference(trained_model, X_featurized)
+    preds_value_decode = label_binarize.inverse_transform(preds_value)[0]
+    return {"prediction" : preds_value_decode}
